3c0cd344f2ae724d5e2831ceee7c7508b23cf09c
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-25 20:41:16 +0300

    Draw sprite

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import  'phaser';
-function create() {
-  let circle1 = this.add.circle(50, 100, 90, 0xfff070);
-  let circle2 = this.add.circle(95, 300, 80, 0xff0000);
-  let circle3 = this.add.circle(200, 200, 100, 0x9f00d0);
-  let circle4 = this.add.circle(300, 400, 10, 0x00e030);
+function preload() {
+  // Load in the sprite here!
 
-  // Add in a circle here!
+  this.load.image(
+    'codey',
+    'https://content.codecademy.com/courses/learn-phaser/codey.png'
+  );
+}
+
+function create() {
+  // Create a sprite game object here!
+  this.add.sprite(40, 80, 'codey');
 }
 
 const config = {
   type: Phaser.AUTO,
   width: 450,
   height: 600,
+  backgroundColor: '#5f2a55',
   scene: {
     create,
+    preload,
   },
 };
 

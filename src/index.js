622ad715d7e1f7202d0fa05ad2b079fb89a9c6bc
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-25 22:43:44 +0300

    Add text in create method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import  'phaser';
+
+const container = document.querySelector('.container')
 function preload() {
   // Load in the sprite here!
 
@@ -6,16 +8,20 @@ function preload() {
     'codey',
     'https://content.codecademy.com/courses/learn-phaser/codey.png'
   );
+
   this.load.image(
     'sky',
     'https://content.codecademy.com/courses/learn-phaser/sky.jpg'
-  );
-}
-
-function create() {
-  // Create a sprite game object here!
+    );
+  }
+  
+  function create() {
+    
+    // Put the background image here!
   this.add.image(200, 200, 'sky')
+  // Create a sprite game object here!
   this.add.sprite(40, 80, 'codey');
+  this.add.text(50, 500, "MY Game")
 }
 
 const config = {
@@ -23,6 +29,7 @@ const config = {
   width: 450,
   height: 600,
   backgroundColor: '#5f2a55',
+  parent: container,
   scene: {
     create,
     preload,

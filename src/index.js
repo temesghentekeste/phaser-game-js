f0d70987d16ed07e1f97686bf96bf1ef5f2bf7d7
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-25 18:03:16 +0300

    Add game object: circles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import './main.scss';
+import  'phaser';
+function create() {
+  let circle1 = this.add.circle(50, 100, 90, 0xfff070);
+  let circle2 = this.add.circle(95, 300, 80, 0xff0000);
+  let circle3 = this.add.circle(200, 200, 100, 0x9f00d0);
+  let circle4 = this.add.circle(300, 400, 10, 0x00e030);
 
-console.log('Running');
+  // Add in a circle here!
+}
+
+const config = {
+  type: Phaser.AUTO,
+  width: 450,
+  height: 600,
+  scene: {
+    create,
+  },
+};
+
+const game = new Phaser.Game(config);

b9095b996b8a01369f6b7fb8f72fab22fbec6954
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-25 20:49:18 +0300

    Add background image

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ function preload() {
     'codey',
     'https://content.codecademy.com/courses/learn-phaser/codey.png'
   );
+  this.load.image(
+    'sky',
+    'https://content.codecademy.com/courses/learn-phaser/sky.jpg'
+  );
 }
 
 function create() {
   // Create a sprite game object here!
+  this.add.image(200, 200, 'sky')
   this.add.sprite(40, 80, 'codey');
 }
 
